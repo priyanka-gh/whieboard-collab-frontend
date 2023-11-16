33c61095bd7f9d83b398012be4db5a8b56a4d414
Priyanka Ghansela <priyanka.ghanselaa@gmail.com>
2023-11-16 18:29:36 +0530

    Enabled Undo Redo Collab

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -9,7 +9,7 @@ const Forms = ({uuid, socket, setUser}) => {
                 <h1 className="fw-bold" style={{color: "#796b6b"}}>Create Room</h1>
                 <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser}/>
             </div>
-            <div className="form-box col-md-4 mx-auto rounded-2 p-5 px-5 d-flex flex-column align-items-center"  style={{backgroundColor: "#efefef"}}>
+            <div className="form-box2 col-md-4 mx-auto border border-2 rounded-2 p-5 px-5 d-flex flex-column align-items-center justify-content-center">
                 <h1 className="fw-bold" style={{color: "#796b6b"}}>Join Room</h1>
                 <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser}/>
             </div>
